24d623e9d9a30b5f1bd029a27aec81a5c28bab08
Ilyas355 <husseinilyas5@gmail.com>
2025-04-16 17:52:55 +0100

    feature: add the Room model to store the rooms in the database

--- chat_and_tasks/models.py
+++ chat_and_tasks/models.py
@@ -16,3 +16,13 @@ class Message(models.Model):
 
     def __str__(self):
         return f"{self.username}: {self.message_content}"
+
+
+class Room(models.Model):
+    name = models.CharField(max_length=50)
+    # for url
+    slug = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return str(self.name)
+
