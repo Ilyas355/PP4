e74e9e769c02c636d3cbaf02cb87f204cba57d57
Ilyas355 <husseinilyas5@gmail.com>
2025-04-16 17:55:57 +0100

    feature: add the task model to store all user tasks

--- chat_and_tasks/models.py
+++ chat_and_tasks/models.py
@@ -26,3 +26,14 @@ class Room(models.Model):
     def __str__(self):
         return str(self.name)
 
+
+class Task(models.Model):
+    name = models.ForeignKey(User, on_delete=models.CASCADE)
+    taskContent = models.CharField(max_length=50)
+    complete = models.BooleanField(default=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+    class Meta:
+        ordering = ('date_added', )
+
+    def __str__(self):
+        return f"{self.name}: {self.taskContent}"
