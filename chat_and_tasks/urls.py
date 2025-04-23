0d646b04736a0827ed6fb3db46651bfb6bcc198b
Ilyas355 <husseinilyas5@gmail.com>
2025-04-23 16:36:24 +0100

    feature: add the url path for the views function delete_object_function

--- chat_and_tasks/urls.py
+++ chat_and_tasks/urls.py
@@ -5,4 +5,5 @@ from . import views as chat_views
 urlpatterns = [
     path('', chat_views.chat_home, name='chat-home'),
     path('<str:room_name>/', chat_views.chat_room, name='chat-room'),
+    path('chatroom/<int:id>', chat_views.delete_object_function, name='delete_object'),
 ]
