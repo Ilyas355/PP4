c68e797b16d7ffb614a034604fb66f8b3cfbc6f9
Ilyas355 <husseinilyas5@gmail.com>
2025-04-24 21:50:44 +0100

    feature: add to the task card showing tasks with deleting and filtering enabled

--- tasks/urls.py
+++ tasks/urls.py
@@ -4,5 +4,5 @@ from . import views as chat_views
 
 urlpatterns = [
     path('tasks/', chat_views.tasks_home, name='Tasks-home'),
-    
+    path('delete_task/<int:task_id>/', chat_views.delete_task, name='delete_task'),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404
 from django.shortcuts import render
 from django.utils.timezone import now, timedelta
 from django.shortcuts import render, redirect
@@ -7,8 +8,34 @@ from .models import Task
 
 # Create your views here.
 
+def filter_option_function(filter, tasks):
+    print(filter)
+    if filter == "completed":
+        return tasks.filter(complete=True)
+    elif filter == "incomplete":
+        return tasks.filter(complete=False)
+    elif filter == "week":
+        return tasks.filter(date_added__gte=now() - timedelta(days=7))
+    elif filter== "month":
+        return tasks.filter(date_added__gte=now() - timedelta(days=30))
+    else:
+        return tasks
 
 @login_required()
 def tasks_home(request):
+    filter_option = request.GET.get("filter", "all")
+    tasks = Task.objects.filter(name=request.user)
+    tasks = filter_option_function(filter_option, tasks)
+    print(request.user.username)
 
-    return render(request, 'tasks/tasks.html')
+
+    return render(request, 'chat/Tasks.html', {'tasks': tasks, 'username': request.user.username, 'date_added': [task.date_added for task in tasks], 'selected_filter': filter_option})
+
+
+@login_required()
+def delete_task(request, task_id):
+    task = get_object_or_404(Task, id=task_id)
+
+    task.delete()
+    
+    return redirect("Tasks-home")
