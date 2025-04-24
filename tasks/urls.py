f2eb124d16f685d29aff39e468fc654713ceac93
Ilyas355 <husseinilyas5@gmail.com>
2025-04-24 21:52:11 +0100

    feature: add the functionality of editing tasks

--- tasks/urls.py
+++ tasks/urls.py
@@ -5,4 +5,5 @@ from . import views as chat_views
 urlpatterns = [
     path('tasks/', chat_views.tasks_home, name='Tasks-home'),
     path('delete_task/<int:task_id>/', chat_views.delete_task, name='delete_task'),
+    path('edit_task/<int:task_id>/', chat_views.edit_task, name='edit_task'),
 ]
