2b0dd1bd83ee087989390d15d53956238ffb7de3
Ilyas355 <husseinilyas5@gmail.com>
2025-04-24 19:58:53 +0100

    feature: adjust the url configurations and the write basic layout of the task.html page

--- tasks/urls.py
+++ tasks/urls.py
@@ -2,3 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from . import views as chat_views
 
+urlpatterns = [
+    path('tasks/', chat_views.tasks_home, name='Tasks-home'),
+    
+]
--- tasks/views.py
+++ tasks/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
+from django.utils.timezone import now, timedelta
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
+from .models import Task
 
 # Create your views here.
+
+
+@login_required()
+def tasks_home(request):
+
+    return render(request, 'tasks/tasks.html')
