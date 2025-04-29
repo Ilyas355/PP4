23273f9982a23b1c3121fdf578433efb8b8a1bdf
Ilyas355 <husseinilyas5@gmail.com>
2025-04-29 15:45:50 +0100

    fix: adjust asgi.py file

--- productivity_app/asgi.py
+++ productivity_app/asgi.py
@@ -1,32 +1,23 @@
-"""
-ASGI config for productivity_app project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
-"""
-
+# mysite/asgi.py
 import os
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'productivity_app.settings')
-
-from django.core.asgi import get_asgi_application
-# ******************** 
-import chat.routing
 
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 
-# ******************** #
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "productivity_app.settings")
+# Initialize Django ASGI application early to ensure the AppRegistry
+# is populated before importing code that may import ORM models.
+django_asgi_app = get_asgi_application()
 
-application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
-    "websocket": AuthMiddlewareStack(
-        URLRouter(
-            chat.routing.websocket_urlpatterns
-        )
-    )
-})
+from chat.routing import websocket_urlpatterns
+
+application = ProtocolTypeRouter(
+    {
+        "http": django_asgi_app,
+        "websocket": AllowedHostsOriginValidator(
+            AuthMiddlewareStack(URLRouter(websocket_urlpatterns))
+        ),
+    }
+)
