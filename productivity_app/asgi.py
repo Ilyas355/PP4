9e8d2dc8cc8e42ce3f63a2dc0c331e9373c89262
Ilyas355 <husseinilyas5@gmail.com>
2025-05-06 15:02:15 +0100

    fix: fix error in heroku

--- productivity_app/asgi.py
+++ productivity_app/asgi.py
@@ -7,6 +7,7 @@ from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "productivity_app.settings")
+django_asgi_app = get_asgi_application()
 # Initialize Django ASGI application early to ensure the AppRegistry
 # is populated before importing code that may import ORM models.
 
@@ -14,7 +15,7 @@ from chat.routing import websocket_urlpatterns
 
 application = ProtocolTypeRouter(
     {
-        "http": get_asgi_application(),
+        "http": django_asgi_app,
         "websocket": AllowedHostsOriginValidator(
             AuthMiddlewareStack(URLRouter(websocket_urlpatterns))
         ),
