288ee5aa2970a08c8c84292b3dfb5a86359985e4
Ilyas355 <husseinilyas5@gmail.com>
2025-07-16 23:32:21 +0100

    fix: debugging redis connection error

--- env.py
+++
@@ -1,4 +0,0 @@
-import os
-os.environ['CLOUDINARY_URL'] = 'cloudinary://597764476196853:ZDvoil9sOlcGbJYvv_mlA-4dZF0@dvz5f3zvw'
-os.environ['DATABASE_URL'] = 'postgresql://neondb_owner:npg_oKR3lnP1YpmB@ep-winter-waterfall-a2ck33yk.eu-central-1.aws.neon.tech/push_cleft_brick_969651'
-os.environ['REDISCLOUD_URL'] = 'redis://default:7ub71zgDTCEKtJfwQ7IBA6VUQLIBX1cR@redis-14221.c6.eu-west-1-1.ec2.redns.redis-cloud.com:14221'
